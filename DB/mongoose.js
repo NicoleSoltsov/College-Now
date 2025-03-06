1d153334c95533cfe7890edac43d82971bd934f4
NicoleSoltsov <113133856+NicoleSoltsov@users.noreply.github.com>
2025-03-06 14:49:54 -0500

    fdfd

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose
-  .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(process.env.MONGO_URI, { useUnifiedTopology: true})
   .then(() => console.log("connected to DB"));
 
 mongoose.connection.on("error", (err) => {
