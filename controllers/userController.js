966842429b0d5714555f1764ed1b5e328aa9265b
NicoleSoltsov <113133856+NicoleSoltsov@users.noreply.github.com>
2025-03-06 10:57:42 -0500

    hgg

--- controllers/userController.js
+++
@@ -1,9 +0,0 @@
-exports.homePage = (req, res) => {
-    const users = ["Tina", "Angie", "Irene"];
-    try {
-      res.json(users);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
