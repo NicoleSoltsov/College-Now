e0cf4042d845ddd0484b9c2dafbb1aa32e9bf7a9
NicoleSoltsov <113133856+NicoleSoltsov@users.noreply.github.com>
2025-03-06 14:22:02 -0500

    dsds

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose
-  .connect(process.env.MONGO_URI, {})
+  .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("connected to DB"));
 
 mongoose.connection.on("error", (err) => {
   console.error(`${err.message}`);
 });
+  
